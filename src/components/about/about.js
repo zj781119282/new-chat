2453fb363ba2856d426670f265528c00235c0fa6
jeffery-zhang <jefferyzhang1013@gmail.com>
2018-08-03 10:38:32 +0800

    update white paper

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,21 +2,17 @@ import $ from 'jquery'
 import './about.scss'
 
 $(() => {
-  const lang = localStorage.getItem('_lang');
+  const lang = localStorage.getItem('_lang') === 'zh-CN' ? 'cn' : 'en';
 
-  function getPdfUrl(type) {
-    let url = 'static/assets/';
-    let pdfName = '';
-
-    if (type === 'tech') {
-      pdfName = lang === 'zh-CN' ? 'tech_white_paper_cn.pdf' : 'tech_white_paper_en.pdf';
-    }
-
+  function getPdfUrl(lang, type) {
+    const url = 'static/assets/';
+    const pdfName = type === 'tech' ? 'tech_white_paper_en.pdf' : `${type}_white_paper_${lang}.pdf`;
+console.log(pdfName);
     return url + pdfName;
   }
 
-  $('.tech-paper').click(() => {
-    const url = getPdfUrl('tech');
+  $('.open-white-paper').click(function() {
+    const url = getPdfUrl(lang, $(this).data('type'));
     window.open(url);
   })
 });
